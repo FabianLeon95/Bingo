f33071eb30d4ecbc66e0f8e0bf4189d4e42e0c80
Allan Amador Alemán <aamador-aleman@outlook.com>
2018-12-21 11:02:41 -0600

    Trabajos en MainWindow. Parte Gráfica. Esta muy básica hoy hago la parte para que meta la matriz en el TextBox

--- Bingo/MainWindow.xaml.cs
+++ Bingo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bingo.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Bingo
 {
     /// <summary>
@@ -22,7 +24,45 @@ namespace Bingo
     {
         public MainWindow()
         {
+
             InitializeComponent();
+        }
+
+        private void btnGivemeCard_Click(object sender, RoutedEventArgs e)
+        {
+            BingoService bs = new BingoService(75);
+            txtCard.Text = "";
+            PositionCardboard[,] cardboard = bs.DealCardboard();
+
+            bool win = bs.ValidateGame(cardboard, Modes.Corner);
+            int count = 0;
+            while (!win && count < 75)
+            {
+                System.Threading.Thread.Sleep(50);
+                Position p = bs.CallPosition();
+                bs.ValidateCardboard(p, cardboard);
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (cardboard[j, i].Marked)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(cardboard[j, i]);
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.Write(cardboard[j, i]);
+                        }
+                    }
+                }
+                win = bs.ValidateGame(cardboard, Modes.Corner);
+                count++;
+            }
+            if (win) txtStatus.Text = "You Win!!!";
+            else txtStatus.Text = "Loser";
         }
     }
 }
